d0401fe192832184c7b859898adf1c273b70a5f3
Jonathan Herrera <jonathanmercutio@gmail.com>
2018-11-21 22:30:06 -0500

    text animations update

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Jonathan Herrera.", "Developer.", "Entrepreneur.", "Cyclist."],
       typeSpeed: 100,
       loop: true,
     });
